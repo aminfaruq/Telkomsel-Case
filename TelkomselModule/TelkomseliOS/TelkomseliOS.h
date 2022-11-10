69d330d7a48a536b907031c5685dc4bb75ee3484
Amin Faruq <aminfaruk.fa@gmail.com>
2022-11-10 22:55:24 +0800

    Add New Target

---
+++ TelkomselModule/TelkomseliOS/TelkomseliOS.h
@@ -0,0 +1,18 @@
+//
+//  TelkomseliOS.h
+//  TelkomseliOS
+//
+//  Created by Amin faruq on 10/11/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TelkomseliOS.
+FOUNDATION_EXPORT double TelkomseliOSVersionNumber;
+
+//! Project version string for TelkomseliOS.
+FOUNDATION_EXPORT const unsigned char TelkomseliOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TelkomseliOS/PublicHeader.h>
+
+
