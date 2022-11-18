79e2442eeba7c91f2d0b8203615cb75fe06ce8f0
Amin Faruq <aminfaruk.fa@gmail.com>
2022-11-18 16:14:13 +0800

    chance detail controller to MVP

--- TelkomselModule/TelkomseliOS/Detail UI/Controllers/DetailController.swift
+++ TelkomselModule/TelkomseliOS/Detail UI/Controllers/DetailController.swift
@@ -6,9 +6,19 @@
 //
 
 import UIKit
+import RealmSwift
 import TelkomselModule
 
-public final class DetailController: UIViewController {
+public final class DetailController: UIViewController  {
+    private lazy var store: FeedStore = {
+        let realm = try! Realm()
+        return  RealmDataFeedStore(realm: realm)
+    }()
+    
+    private lazy var localFeedLoader: LocalFeedLoader = {
+        LocalFeedLoader(store: store )
+    }()
+    
     @IBOutlet private(set) public var detailImageView: UIImageView!
     @IBOutlet private(set) public var backgroudView: UIView!
     @IBOutlet private(set) public var detailNameLabel: UILabel!
@@ -17,30 +27,22 @@ public final class DetailController: UIViewController {
     @IBOutlet private(set) public var detailPublisherLabel: UILabel!
     @IBOutlet private(set) public var detailLatestVersionLabel: UILabel!
     @IBOutlet private(set) public var detailActionBtn: UIButton!
-
-    var viewModel: DetailControllerVM!
-
+    
+    lazy var presenter = DetailViewPresenter(delegate : self)
+    
     public var item: FeedItem!
+    
     private var buttonIsSelected = false
-
+    
     public override func viewDidLoad() {
         super.viewDidLoad()
-
-        viewModel = DetailControllerVM()
-
-        detailImageView.setImageFromURL(stringUrl: item.productLogo)
-        backgroudView.backgroundColor = UIColor.hexStringToUIColor(hex: "#\(item.colorTheme)")
-        detailNameLabel.text = item.productName
-        detailDescriptionLabel.text = item.description
-        detailRatingLabel.text = "Rating: \(item.rating)"
-        detailPublisherLabel.text = item.publisher
-        detailLatestVersionLabel.text = "Version: \(item.latestVersion)"
         
+        presenter.showItem(item: item)
         bindAction()
     }
     
     private func bindAction() {
-        if viewModel.isProductSaved(productName: item.productName) {
+        if presenter.showProductByName(productName: item.productName) {
             detailActionBtn.backgroundColor = .systemRed
             detailActionBtn.setTitle("Delete", for: .normal)
             buttonIsSelected = true
@@ -55,13 +57,39 @@ public final class DetailController: UIViewController {
         if buttonIsSelected {
             detailActionBtn.backgroundColor = .systemRed
             detailActionBtn.setTitle("Delete", for: .normal)
-            viewModel.deleteProduct(productName: item.productName)
+            presenter.deleteProduct(productName: item.productName)
         } else {
             detailActionBtn.backgroundColor = .systemBlue
             detailActionBtn.setTitle("Save", for: .normal)
-            viewModel.saveProduct(item: item)
+            presenter.saveProduct(item: item)
         }
+        
         bindAction()
     }
+    
 }
 
+extension DetailController: DetailViewDelegate {
+    
+    public func didShowItem(item: FeedItem) {
+        detailImageView.setImageFromURL(stringUrl: item.productLogo)
+        backgroudView.backgroundColor = UIColor.hexStringToUIColor(hex: "#\(item.colorTheme)")
+        detailNameLabel.text = item.productName
+        detailDescriptionLabel.text = item.description
+        detailRatingLabel.text = "Rating: \(item.rating)"
+        detailPublisherLabel.text = item.publisher
+        detailLatestVersionLabel.text = "Version: \(item.latestVersion)"
+    }
+    
+    public func didSaveProduct(item: FeedItem) {
+        try? localFeedLoader.save(item)
+    }
+    
+    public func didDeleteProduct(productName: String) {
+        try? localFeedLoader.deleteCacheFeed(productName)
+    }
+    
+    public func didShowProductByName(productName: String) -> Bool {
+        try! localFeedLoader.isProductSaved(productName)
+    }
+}
---
+++ TelkomselModule/TelkomseliOS/Detail UI/Controllers/DetailViewPresenter.swift
@@ -0,0 +1,40 @@
+//
+//  DetailControllerVM.swift
+//  TelkomseliOS
+//
+//  Created by Amin faruq on 13/11/22.
+//
+
+import Foundation
+import TelkomselModule
+
+public protocol DetailViewDelegate: NSObjectProtocol {
+    func didShowItem(item: FeedItem)
+    func didSaveProduct(item: FeedItem)
+    func didDeleteProduct(productName: String)
+    func didShowProductByName(productName: String) -> Bool
+}
+
+class DetailViewPresenter {
+    weak private var delegate : DetailViewDelegate?
+
+    init(delegate: DetailViewDelegate){
+        self.delegate = delegate
+    }
+    
+    func showItem(item: FeedItem){
+        delegate?.didShowItem(item: item)
+    }
+    
+    func saveProduct(item: FeedItem) {
+        delegate?.didSaveProduct(item: item)
+    }
+    
+    func deleteProduct(productName: String) {
+        delegate?.didDeleteProduct(productName: productName)
+    }
+    
+    func showProductByName(productName: String) -> Bool {
+        delegate?.didShowProductByName(productName: productName) ?? false
+    }
+}
